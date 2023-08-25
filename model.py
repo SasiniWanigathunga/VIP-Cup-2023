77f3eedb61b38591ac78753431ad701f6a58d21f
Sasini Wanigathunga <92870335+SasiniWanigathunga@users.noreply.github.com>
2023-08-25 22:01:13 +0530

    Update model.py

--- model.py
+++ model.py
@@ -8,20 +8,40 @@ class ResNet(nn.Module):
     """encoder + classifier"""
     def __init__(self, name='resnet152', num_classes=2):
         super(ResNet, self).__init__()
-        if name == 'resnet152':
+        if name == 'vgg19':
+            self.encoder = torchvision.models.vgg19(pretrained=True)
+            self.encoder.features[0] = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+            self.encoder.classifier[6] = nn.Identity()
+            self.fc = nn.Linear(4096, num_classes)
+        elif name=='resnet152':
+            self.encoder = torchvision.models.resnet152(zero_init_residual=True)
+            self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+            self.encoder.fc = nn.Identity()
+            self.fc = nn.Linear(2048, num_classes)
+        elif name=='resnet101':
+            self.encoder = torchvision.models.resnet101(zero_init_residual=True)
+            self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+            self.encoder.fc = nn.Identity()
+            self.fc = nn.Linear(2048, num_classes)
+        elif name=='resnet50':
+            self.encoder = torchvision.models.resnet50(zero_init_residual=True)
+            self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+            self.encoder.fc = nn.Identity()
+            self.fc = nn.Linear(2048, num_classes)
+        elif name=='resnet18':
+            self.encoder = torchvision.models.resnet18(zero_init_residual=True)
+            self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+            self.encoder.fc = nn.Identity()
+            self.fc = nn.Linear(512, num_classes)
+        elif name=='unet':
+            self.encoder = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet', in_channels=3, out_channels=num_classes, init_features=32, pretrained=True)
+            #self.encoder = UNet(in_channels=1, out_channels=num_classes)
+            self.fc = nn.Identity()
+        else:
             self.encoder = torchvision.models.resnet152(pretrained=True)
             self.encoder.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
             self.encoder.fc = nn.Identity()
             self.fc = nn.Linear(2048, num_classes)
-        elif name=='unet':
-            self.encoder = torch.hub.load('mateuszbuda/brain-segmentation-pytorch', 'unet',    in_channels=3, out_channels=num_classes, init_features=32, pretrained=True)
-            #self.encoder = UNet(in_channels=1, out_channels=num_classes)
-            self.fc = nn.Identity()
-        else:
-            self.encoder = torchvision.models.vgg19(zero_init_residual=True)
-            self.encoder.features[0] = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-            self.encoder.classifier[6] = nn.Identity()
-            self.fc = nn.Linear(4096, num_classes)
         
     def forward(self, x):
 
